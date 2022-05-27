fd0dd3a1ae19a7b153048109946a3c15cd3ac925
Bigro98 <62119813+imohetta@users.noreply.github.com>
2022-05-28 00:52:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word = input("input your word: "), anagram = input("input the test word: ")):
+    
+    
+    wordletters = sorted(word)
+    # print(wordletters)
+    
+    anagramletters = sorted(anagram)
+    # print(anagramletters)
+
+
+    if  wordletters==anagramletters:
+        print("The words are anagrams")
+        return True
+    
+    else:
+        print("The words are not anagrams.")
+        return
+        
+find_anagram()
